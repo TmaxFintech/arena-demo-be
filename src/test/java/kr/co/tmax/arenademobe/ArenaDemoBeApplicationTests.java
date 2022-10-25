1dc825b35576844866add55234417ae427dae816
Hott-J <jhj13062004@naver.com>
2022-10-25 20:12:48 +0900

    #3 - 메인 메소드
    메인 메소드

---
+++ src/test/java/kr/co/tmax/arenademobe/ArenaDemoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.tmax.arenademobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArenaDemoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
